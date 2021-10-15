ebb1e049f1fcbc847eb03222dce08ffe8f4700a2
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2021-10-15 19:14:25 +0700

    Added route to controller call

--- app/core/App.php
+++ app/core/App.php
@@ -4,12 +4,35 @@
 
 class App
 {
-	// TODO: Implement App stuff here
+	protected $controller = "Home";
+	protected $method = "index";
+	protected $params = [];
+
 	public function __construct()
 	{
 		// echo "App is instantiated";
 		$url = $this->parseURL();
-		var_dump($url);
+		// var_dump($url);
+		if (file_exists("../app/controllers/" . $url[0] . ".php")) {
+			$this->controller = $url[0];
+			unset($url[0]);
+			// var_dump($url);
+		}
+
+		require_once "../app/controllers/" . $this->controller . ".php";
+		$this->controller = new $this->controller;
+
+		if (isset($url[1])) {
+			if (method_exists($this->controller, $url[1])) {
+				$this->method = $url[1];
+				unset($url[1]);
+			}
+		}
+
+		if (!empty($url)) {
+			$this->params = $url;
+			var_dump($url);
+		}
 	}
 
 	public function parseURL()
