1a7286b90c12f1054da1a9b5ab6a7ce737fb9c14
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2021-11-05 19:32:20 +0700

    Templating engine now can send variables

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -2,10 +2,11 @@
 
 class About extends Controller
 {
-	public function page()
+	public function page($param1 = "", $param2 = "")
 	{
+		$title = "Foo bar";
 		$this->view("templates/header");
-		$this->view("about/index");
+		$this->view("about/index", compact("param1", "param2", "title"));
 		$this->view("templates/footer");
 	}
 }
