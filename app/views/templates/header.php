d22b9959aef5d54098ecfb105208cbe60f29a940
Bondan Rachmat Santoso <bondanrsantoso@gmail.com>
2021-11-15 19:59:46 +0700

    Sync up with latest course

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,8 +5,23 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="<?php constant("BASEURL"); ?>css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php BASEURL ?>css/bootstrap.min.css">
 	<title>Foo</title>
 </head>
 
 <body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<a href="#" class="navbar-brand">Pemrograman Web MVC</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse">
+				<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+					<li class="nav-item"><a href="<?php BASEURL ?>/Home/index" class="nav-link">Home</a></li>
+					<li class="nav-item"><a href="<?php BASEURL ?>/About/page" class="nav-link">About</a></li>
+					<li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
